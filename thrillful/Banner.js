985871c65183e8e6523a840b9466a6fa8ae696de
Sandi <59848573+GypsyJasmer@users.noreply.github.com>
2021-04-05 12:02:52 -0700

    Updated banner page, removed footer and will fix later. moved function to JS file. cleaned the layout of code. Now working on banner replacement.

--- thrillful/Banner.js
+++ thrillful/Banner.js
@@ -1,13 +1,8 @@
-
-
-function showPreviewOne(event) {
-    if (event.target.files.length > 0) {
-        let src = URL.createObjectURL(event.target.files[0]);
-        let preview = document.getElementById("file-ip-1-preview");
-        preview.src = src;
-        preview.style.display = "block";
-    }
-}
-function myImgRemoveFunctionOne() {
-    document.getElementById("file-ip-1-preview").src = "https://i.ibb.co/ZVFsg37/default.png";
-}
+// This function is only to click on image and expand. 
+        function myFunction(imgs) {
+            var expandImg = document.getElementById("expandedImg");
+            var imgText = document.getElementById("imgtext");
+            expandImg.src = imgs.src;
+            imgText.innerHTML = imgs.alt;
+            expandImg.parentElement.style.display = "block";
+        }
