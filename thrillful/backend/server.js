c423cd84dd30fa10a4f0c3a3a9af3d00951fad9e
Sandi <59848573+GypsyJasmer@users.noreply.github.com>
2021-04-22 12:13:05 -0700

    Watched the node js video and added some comments to server.js to make a port varaible later

--- thrillful/backend/server.js
+++ thrillful/backend/server.js
@@ -11,6 +11,11 @@ app.get('/', (req, res) => {
     res.send('Server is ready');
 });
 
+//const port = process.env.PORT || 5000
+
 app.listen(5000, () => {
     console.log('Serve at http://localhost:5000');
+
+    //Template literal need back tic char instead of single quotes. 
+    //console.log('Serve at http://localhost:${port}');
 })
