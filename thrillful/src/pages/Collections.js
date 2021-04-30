af52d37e7a069365d5c1b0b215d54ab96fea3e3a
Georgia Fox <foxgc@my.lanecc.edu>
2021-04-29 20:10:02 -0700

    clean up comments

--- thrillful/src/pages/Collections.js
+++ thrillful/src/pages/Collections.js
@@ -22,7 +22,7 @@ const Collections = () => {
             <Grid templateColumns={["repeat(1, 1fr)", "repeat(3, 1fr)"]} m="auto">
                 {
                     collections.map(collection => (
-                        <Link to={`/${collection.handle}`} key={collection.id}>
+                        <Link to={`collection/${collection.id}`} key={collection.id}>
                             <Box _hover={{ opacity: '80%' }} textAlign='center'/*move text here*/>
                                 <Image src={collection.image.src} p="2rem" h="25rem" w="25rem" m="2.5rem" /> 
                                <Center>
